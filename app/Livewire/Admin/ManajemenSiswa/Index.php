2b621e80cdff2f04077cd58286d26ac34d746edb
wildun19 <wildanabdunnaseer19@gmail.com>
2025-05-06 11:08:59 +0700

    CRUD popup

--- app/Livewire/Admin/ManajemenSiswa/Index.php
+++ app/Livewire/Admin/ManajemenSiswa/Index.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\ManajemenSiswa;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,9 +14,101 @@ class Index extends Component
     public $paginate = "10";
     public $search = '';
 
+    public $name, $email, $password, $password_confirmation;
+    public $user_id;
+
+    public function create(){
+        $this->resetValidation();
+        $this->reset([
+            'name',
+            'email',
+            'password',
+            'password_confirmation',
+        ]);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required',
+        ], [
+            'name.required' => 'Name Tidak Boleh Kosong',
+            'email.required' => 'Email Tidak Boleh Kosong',
+            'email.email' => 'Email Tidak Valid',
+            'email.unique' => 'Email Sudah terdaftar',
+            'password.required' => 'passsword Tidak Boleh Kosong',
+            'password.min' => 'passsword Minimal 8 Karakter',
+            'password.confirmed' => 'passsword Konfirmasi Tidak Sama',
+            'password_confirmation.required' => 'Password Konfirmasi Tidak Boleh Kosong',
+        ]);
+
+        $siswa = new User;
+        $siswa->name = $this->name;
+        $siswa->email = $this->email;
+        $siswa->role = 'Siswa';
+        $siswa->password = Hash::make($this->password);
+        $siswa->save();
+
+        $this->dispatch('closeCreateModel');
+    }
+
+    public function edit($id)
+    {
+        $this->resetValidation();
+        $this->reset([
+            'password',
+            'password_confirmation',
+        ]);
+        $siswa = User::findOrFail($id);
+        $this->name = $siswa->name;
+        $this->email = $siswa->email;
+        $this->user_id = $siswa->id;
+    }
+
+    public function update($id)
+    {
+        $siswa = User::findOrFail($id);
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$id,
+            'password' => 'nullable|min:8|confirmed',
+        ], [
+            'name.required' => 'Name Tidak Boleh Kosong',
+            'email.required' => 'Email Tidak Boleh Kosong',
+            'email.email' => 'Email Tidak Valid',
+            'email.unique' => 'Email Sudah terdaftar',
+            'password.min' => 'passsword Minimal 8 Karakter',
+            'password.confirmed' => 'passsword Konfirmasi Tidak Sama',
+        ]);
+
+        $siswa->name = $this->name;
+        $siswa->email = $this->email;
+        $siswa->role = 'Siswa';
+        if ($this->password) {
+            $siswa->password = Hash::make($this->password);
+        }
+        $siswa->update();
+
+        $this->dispatch('closeEditModel');
+    }
+
+    public function destroy($id)
+    {
+        $siswa = User::find($id);
+        if ($siswa) {
+            $siswa->delete();
+
+            $this->dispatch('deleteSuccess');
+        }
+    }
+
     public function render()
     {
         $siswa = array(
+            'title' => 'Managemen User',
             'siswa' => User::where('role', 'Siswa')->where(function($query) {
                 $query->where('name', 'like', '%'.$this->search.'%')
                 ->orWhere('email', 'like', '%'.$this->search.'%');
--- resources/views/components/layouts/script.blade.php
+++ resources/views/components/layouts/script.blade.php
@@ -4,3 +4,5 @@
 <script data-navigate-once src="{{ asset('adminlte3/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script data-navigate-once src="{{ asset('adminlte3/dist/js/adminlte.min.js') }}"></script>
+
+<script src="{{ asset('sweetalert2/dist/sweetalert2.all.min.js') }}"></script>
